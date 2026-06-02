eb6a1e54aba0dc460e5268a33df4a1a0e842a3cf
Nazar Krughliakov <zonnix336@gmail.com>
2026-06-02 12:14:17 +0300

    feat(auth-layout): implement base layout + css

--- src/layouts/AuthLayout/AuthLayout.tsx
+++ src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,9 +1,12 @@
 import {Outlet} from "react-router-dom";
+import styles from './AuthLayout.module.css';
 
 export const AuthLayout = () => {
     return (
-        <main>
-            <Outlet/>
+        <main className={styles.layout}>
+            <div className={styles.card}>
+                <Outlet/>
+            </div>
         </main>
     );
 };
