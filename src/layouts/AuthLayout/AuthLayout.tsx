7df4e56a58e5d093f6563f93a607ea7c0a7b73e5
Nazar Krughliakov <zonnix336@gmail.com>
2026-06-06 14:13:38 +0300

    feat(LoginPage): login page UI done

--- src/layouts/AuthLayout/AuthLayout.tsx
+++ src/layouts/AuthLayout/AuthLayout.tsx
@@ -4,9 +4,16 @@ import styles from './AuthLayout.module.css';
 export const AuthLayout = () => {
     return (
         <main className={styles.layout}>
+            <div className={styles.decorations}>
+                <div className={styles.circle}></div>
+                <div className={styles.circle}></div>
+                <div className={styles.circle}></div>
+            </div>
+
             <div className={styles.card}>
                 <Outlet/>
             </div>
         </main>
     );
 };
+
